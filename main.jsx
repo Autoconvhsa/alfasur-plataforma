87b7081f49c127727ccb56013eb0c1b9eda14a96
Autoconvhsa <nationalpetroleum25@gmail.com>
2026-07-11 12:00:38 -0600

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./alfasur-plataforma3_06.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
