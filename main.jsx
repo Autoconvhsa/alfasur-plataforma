f49e0103865440342fcc13b275f123d1cb04d8ba
Autoconvhsa <nationalpetroleum25@gmail.com>
2026-07-14 23:08:50 -0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./alfasur-plataforma3_06.jsx";
+import App from "./alfasur-plataforma14_07.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
